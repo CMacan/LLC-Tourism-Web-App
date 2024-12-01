0a610ad869e0003d59c2d75c59d998d195f29d9e
CMacan <113482858+CMacan@users.noreply.github.com>
2024-12-01 14:08:25 +0800

    fixed http request urls

--- tourism_project/tourism_project/urls.py
+++ tourism_project/tourism_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tourism_web_app.urls'))
+    path('tourism_web_app/', include('tourism_web_app.urls')),
+    path('', include('tourism_web_app.urls')),
 ]
--- tourism_project/tourism_web_app/urls.py
+++ tourism_project/tourism_web_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tourism_web_app/', views.tourism_web_app, name='tourism_web_app'),
+    path('', views.home, name='home'),
 ]
