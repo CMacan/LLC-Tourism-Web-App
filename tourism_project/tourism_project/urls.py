fcb981386b020af06129f9063d7ee0586cd13a4e
Clarice Franz <150408595+KhaleesiCat@users.noreply.github.com>
2024-12-06 17:40:16 +0800

    yeyeye

--- tourism_project/tourism_project/urls.py
+++ tourism_project/tourism_project/urls.py
@@ -19,6 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tourism_web_app/', include('tourism_web_app.urls')),
     path('', include('tourism_web_app.urls')),
 ]
--- tourism_project/tourism_web_app/urls.py
+++ tourism_project/tourism_web_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('destination/', views.destination, name='destination'),
 ]
