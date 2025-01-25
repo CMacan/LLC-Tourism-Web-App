823382acd3b16d24acb5d84d2cc4f3c69d039d35
AshCloud2123 <150422810+AshCloud2123@users.noreply.github.com>
2025-01-25 10:24:06 +0800

    configure

--- tourism_project/settings.py
+++ tourism_project/settings.py
@@ -163,7 +163,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'js'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
